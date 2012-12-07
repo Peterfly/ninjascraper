24daf619847972aedf1333a3afa29186ac179553
Peter Zhenghao Qian <peterqian@lexity.com>
2012-12-07 00:35:50 -0800

    now it can scrape all upper div cs courses

--- scrape_ninja.py
+++ scrape_ninja.py
@@ -7,6 +7,8 @@ def scrape_hkn(abv="CS",course="70"):
     prof_year = {} 
     html = urllib2.urlopen("https://hkn.eecs.berkeley.edu/coursesurveys/course/{0}/{1}".format(abv,course))
     soup = bs4.BeautifulSoup(html) 
+    if len(soup.find_all("table")) == 0:
+        return None
     tables = soup.find_all("table")[1] 
     links = tables.find_all("a") 
     current_semester = None 
@@ -26,6 +28,8 @@ ways = ["","(solution)","solution"]
 
 def scrape_ninja(course="70",test="Midterm 1",department="COMPSCI",abv="CS"):
     prof_year = scrape_hkn(abv,course)
+    if prof_year == None:
+        return
     base_url = "http://media.ninjacourses.com/var/exams/1/{0}/{1}%20{2}%20-%20{3}%20{4}%20-%20{5}%20-%20{6}%20{7}.pdf" 
     exists = {}  
     for semester,profs in prof_year.iteritems():
@@ -57,9 +61,18 @@ def scrape_ninja(course="70",test="Midterm 1",department="COMPSCI",abv="CS"):
 
 tests = ['Midterm','Midterm 1','Midterm 2', 'Midterm 3','Final']
 
+
 if (len(sys.argv) > 1):
-    for test in tests: 
-        scrape_ninja(sys.argv[1],test) 
+    if sys.argv[1] == "all":
+        for i in range(150, 300):
+            print "scraping cs" + i
+            for test in tests:
+                scrape_ninja(i, test)
+    else:
+        for test in tests:
+            print "scraping cs" + sys.argv[1]
+            scrape_ninja(sys.argv[1],test) 
 else:
     for test in tests: 
         scrape_ninja("70",test)
+        
